177242f3ba93929a8019eeca5bb2d946a27f5e55
Pedrolustosa <pedroeternalss@gmail.com>
2022-12-11 17:05:40 -0300

    feat: Setting the connection string and registering the service

--- InventoryAPI/Program.cs
+++ InventoryAPI/Program.cs
@@ -1,13 +1,20 @@
+using InventoryAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. - ConfigureService
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefautConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, 
+                                                 ServerVersion.AutoDetect(connectionString)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. - Configure
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,29 +22,4 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
