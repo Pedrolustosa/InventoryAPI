4a25cd4a732392fb6bbb11fd0bcc99193b530154
Pedrolustosa <pedroeternalss@gmail.com>
2022-12-11 16:51:13 -0300

    feat: Fluent API - Configuring properties and relationships

--- InventoryAPI/Context/AppDbContext.cs
+++ InventoryAPI/Context/AppDbContext.cs
@@ -9,5 +9,38 @@ namespace InventoryAPI.Context
 
         public DbSet<Product>? Products { get; set; }
         public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            #region Category
+            modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+            modelBuilder.Entity<Category>().Property(c => c.Name)
+                                           .HasMaxLength(100)
+                                           .IsRequired();
+            modelBuilder.Entity<Category>().Property(c => c.Description)
+                                           .HasMaxLength(175)
+                                           .IsRequired();
+            #endregion
+
+            #region Product
+            modelBuilder.Entity<Product>().HasKey(c => c.ProductId);
+            modelBuilder.Entity<Product>().Property(c => c.Name)
+                                           .HasMaxLength(100)
+                                           .IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.Description)
+                                           .HasMaxLength(175)
+                                           .IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.Image)
+                                          .HasMaxLength(100)
+                                          .IsRequired();
+            modelBuilder.Entity<Product>().Property(c => c.Price).HasPrecision(14, 2);
+            #endregion
+
+            #region Relationship
+            modelBuilder.Entity<Product>().HasOne<Category>(c => c.Category)
+                                          .WithMany(p => p.Products)
+                                          .HasForeignKey(c => c.CategoryId);
+            #endregion
+        }
     }
 }
