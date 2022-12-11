d67f9e0e0c8f1f6ee892cc8d5182353a236acf3e
Pedrolustosa <pedroeternalss@gmail.com>
2022-12-11 18:59:26 -0300

    feat: Creating the endpoint to change a category with MapPut

--- InventoryAPI/Program.cs
+++ InventoryAPI/Program.cs
@@ -36,6 +36,22 @@ app.MapPost("/categories", async ([FromBody] Category category, [FromServices] A
   .Produces<Category>(StatusCodes.Status201Created)
   .WithName("NewCategory");
 
+app.MapPut("categories/{id:int}", async (int id, Category category, AppDbContext db) =>
+{
+    if (category.CategoryId != id)
+        return Results.BadRequest();
+
+    var categoryDB = await db.Categories.FindAsync(id);
+
+    if (categoryDB is null) return Results.NotFound();
+
+    categoryDB.Name = category.Name;
+    categoryDB.Description = category.Description;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(category);
+});
+
 #endregion
 
 
