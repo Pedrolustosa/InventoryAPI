245a69e3a363da3dcaf7be1872179252d6a42089
Pedrolustosa <pedroeternalss@gmail.com>
2022-12-11 16:34:49 -0300

    feat: Creating the Context file - AppDbContext

---
+++ InventoryAPI/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? categories { get; set; }
+    }
+}
